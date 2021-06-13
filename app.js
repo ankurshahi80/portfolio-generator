3d90d3399a9f39ccbd7d86aab01359dc7cffef48
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-06-13 19:04:08 -0400

    dynamic HTML template

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const inquirer = require('inquirer');
 const { AnimationFrameScheduler } = require('rxjs/internal/scheduler/AnimationFrameScheduler');
-// const fs = require ('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require ('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
    return inquirer.prompt([
         {
@@ -144,5 +137,11 @@ return inquirer
 promptUser()
     .then(promptProject)
     .then(portfolioData=> {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
