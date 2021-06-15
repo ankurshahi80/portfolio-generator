f195e31b1853fc4f215eca8b5ac06e7af25e4fa5
Ankur <ankurshahi@Ankurs-iMac.phub.net.cable.rogers.com>
2021-06-15 11:35:29 -0400

    refactored project code

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ promptUser()
     .then(portfolioData=> {
         const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('./index.html', pageHTML, err => {
+        fs.writeFile('./dist/index.html', pageHTML, err => {
             if (err) throw err;
 
             console.log('Portfolio complete! Check out index.html to see the output!');
